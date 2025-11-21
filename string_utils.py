a1484017378a09a4795a812f4ac701a993bc8dad
norsultani <norsultani@gmail.com>
2025-11-21 16:26:30 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,11 +1,43 @@
 
 
 
-def split_before_uppercases(formula):
-    pass  # replace the pass with your code
+def split_before_each_uppercases(formula):
+    start = 0
+    split_formula = []
 
-def split_at_digit(formula):
-    pass  # replace the pass with your code
+    if formula == "":
+      return split_formula
+      
+
+    
+    for end in range(1,len(formula)):
+        if formula[end].isupper():
+            split_formula.append(formula[start:end])
+            start = end
+        
+   
+
+    split_formula.append(formula[start:])
+
+    return split_formula
+
+
+def split_at_first_digit(formula):
+  start = 1
+  endlist = []
+
+  for digitornum in formula[1:]:
+    if digitornum.isdigit():
+      break
+    start += 1
+
+  if start == len(formula):
+    return formula, 1
+
+  letters = formula[:start]
+  nums = int(formula[start:])
+
+  return letters, nums
 
 def count_atoms_in_molecule(molecular_formula):
     """Takes a molecular formula (string) and returns a dictionary of atom counts.  
